4b1e88195b6061bf1e81fb44c473007945222e6b
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-05-26 16:22:03 -0300

    chore: starting academic_system module new

---
+++ academic_system/src/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    String name;
+    int age;
+    String gender;
+
+    public Student (String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public static void main(String [] args) {}
+}
